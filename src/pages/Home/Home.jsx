4cef38017d746607d98bc7205b0a2502dcc8304a
tirahman105 <tirahman105@gmail.com>
2023-05-01 23:48:45 +0600

    home banner done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import {  Col, Row } from "react-bootstrap";
+import fish from "../../assets/fish.png";
+
+
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home</h1>
-        </div>
-    );
+  return (
+    <Row className="justify-content-center align-items-center mx-auto">
+      <Col md={6}>
+        <h1>Discover the rich flavors of Bangladeshi cuisine</h1>
+        <p>Taste the cultural diversity of Bangladesh in every dish</p>
+        <button className="btn btn-color">Explore Bangladeshi Foods</button>
+      </Col>
+      <Col md={6}>
+      <img className="img-fluid" src={fish} alt="" />
+      </Col>
+
+      
+    </Row>
+  );
 };
 
 export default Home;
