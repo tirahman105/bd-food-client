af7892ef9e66c2487fe1adba00ef337a988e4083
tirahman105 <tirahman105@gmail.com>
2023-05-01 21:34:48 +0600

    basic routes setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzagvR4vCGu-U9zCX9D2RihHpEqD4Cn9A",
+  authDomain: "bd-food-f4490.firebaseapp.com",
+  projectId: "bd-food-f4490",
+  storageBucket: "bd-food-f4490.appspot.com",
+  messagingSenderId: "486639218576",
+  appId: "1:486639218576:web:39e8797833eab7b6c64d15"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
