f25d109fd6b64996c3defad1aa8fb125f5807b64
tirahman105 <tirahman105@gmail.com>
2023-05-02 23:01:53 +0600

    sign in with email done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Form } from "react-bootstrap";
 import './Login.css'
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
+    const {signIn} = useContext(AuthContext);
+
+    const handleLogin = event => {
+    
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+    
+        console.log( email, password);
+    
+        signIn(email, password) 
+        .then(result => {
+          const loggedUser = result.user;
+          console.log(loggedUser);
+          
+    
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    
+      }
+
+
   return (
     <div className="bg-login">
           <div className="col-md-6 mx-auto border p-4 bg-light rounded ">
         <h3 className="text-center">Login</h3>
-      <Form>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control name="email" type="email" placeholder="Enter email" required />
